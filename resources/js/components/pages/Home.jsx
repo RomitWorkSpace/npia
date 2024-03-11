3f52347287cb810f3f4971e256a6c855c137cc92
RomitWorkSpace <romitkumar848@gmail.com>
2024-03-11 09:51:20 +0530

    NPIA Project start

---
+++ resources/js/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+        <h1>Hello NPIA</h1>
+        <div className='' style={{height:'900px'}}></div>
+        </>
+    )
+}
+
+export default Home
