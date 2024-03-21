46d78a97ceebf8c4ca362984c6eaadce15392941
RomitWorkSpace <romitkumar848@gmail.com>
2024-03-21 16:53:32 +0530

    other pages added

--- resources/js/common/Layout.jsx
+++ resources/js/common/Layout.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from '../components/pages/Home';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import About from '../components/pages/About'
+import Contact from '../components/pages/Contact'
+import OurProduct from '../components/pages/OurProduct';
+import ProductDetail from '../components/pages/ProductDetail';
 
 
 function Layout() {
@@ -11,11 +15,12 @@ function Layout() {
     <Navbar />
     <Routes>
         <Route path="/" exact element={<Home/>} />
-        {/* <Route path="/about" exact element={<About/>} />
-        <Route path="/portfolio" exact element={<Portfolio/>} />
-        <Route path="/services"  element={<Service />} />
-        <Route path='/service/:slug' exact element={<SubService/>} />
-        <Route path="/contact"  element={<Contact />} /> */}
+        <Route path="/about" exact element={<About/>} />
+        <Route path="/products" exact element={<OurProduct />} />
+        <Route path="/product/:slug" exact element={<ProductDetail />} />
+        {/* <Route path="/services"  element={<Service />} />
+        <Route path='/service/:slug' exact element={<SubService/>} /> */}
+        <Route path="/contact"  element={<Contact />} /> 
     </Routes>
     <Footer />
     </>
