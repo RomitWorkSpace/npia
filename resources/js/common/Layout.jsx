1548d1e00784d5cbfb4109f2dfa0a165c2254c78
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-15 09:43:59 +0530

    Industry page added

--- resources/js/common/Layout.jsx
+++ resources/js/common/Layout.jsx
@@ -7,6 +7,7 @@ import About from '../components/pages/About'
 import Contact from '../components/pages/Contact'
 import OurProduct from '../components/pages/OurProduct';
 import ProductDetail from '../components/pages/ProductDetail';
+import IndustryPage from '../components/pages/IndustryPage';
 
 
 function Layout() {
@@ -18,6 +19,7 @@ function Layout() {
         <Route path="/about" exact element={<About/>} />
         <Route path="/products" exact element={<OurProduct />} />
         <Route path="/product/:slug" exact element={<ProductDetail />} />
+        <Route path="/industry/:slug" exact element={<IndustryPage />} />
         {/* <Route path="/services"  element={<Service />} />
         <Route path='/service/:slug' exact element={<SubService/>} /> */}
         <Route path="/contact"  element={<Contact />} /> 
