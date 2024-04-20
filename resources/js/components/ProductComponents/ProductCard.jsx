7b19966a5e2a20fcc4e9bdffb30ff9c816e2014a
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-20 10:23:26 +0530

    Link added on image

--- resources/js/components/ProductComponents/ProductCard.jsx
+++ resources/js/components/ProductComponents/ProductCard.jsx
@@ -5,7 +5,9 @@ function ProductCard({props}) {
     return (
         <>
         <div className='col-md-3 text-center mb-4'>
-            <img src={`https://filmcitynow.com/storage/app/${props.image}`} width="100%" alt="product item" />
+            <Link to={`/product/${props.slug}`}>
+                <img src={`https://filmcitynow.com/storage/app/${props.image}`} width="100%" alt="product item" />
+            </Link>
             <Link to={`/product/${props.slug}`}>
             <span className=''>{props.name}</span>
             </Link>
