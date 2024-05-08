d254fa81f18dabeb8be09ec79ade5d7e4ccf91af
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-08 19:12:31 +0530

    converted into industry slider

--- resources/js/components/HomeComponents/NumberStrip.jsx
+++ resources/js/components/HomeComponents/NumberStrip.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import NumberBg from 'imgPath/Number-Bg.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+import { Pagination, Autoplay } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function NumberStrip() {
+    const [industry, setIndustry] = useState([]);
+
+    useEffect(() => {
+
+        axios.get('/api/industries').then(res => {
+            setIndustry(res.data.industries);
+        })
+    },[]);
+
+    var industry_slider = "";
+
+    industry_slider = industry.map( (item) => {
+        return (
+            <>
+                <SwiperSlide>
+                    <Link to={`/industry/${item.slug}`}>
+                        <img src={`/storage/app/${item.image}`} width="100%" />
+                        <div className='text-center pt-3'><p>{item.name}</p></div>
+                    </Link>
+                </SwiperSlide>
+            </>
+        )
+    })
+
     return (
         <>
-        <div className='container-fluid strip-bg pt-3 pb-5' style={{backgroundImage: `url(${NumberBg})`}}>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-3 col-6 mt-3 text-center'>
-                        <h1 className='clr-white bold'>1250<span style={{color:'#f5a005'}}>K</span></h1>
-                        <span className='clr-white bold'>Satisfy Customers</span>
-                    </div>
-                    <div className='col-md-3 col-6 mt-3 text-center'>
-                        <h1 className='clr-white bold'>50<span style={{color:'#f5a005'}}>K</span></h1>
-                        <span className='clr-white bold'>Technical Teams</span>
-                    </div>
-                    <div className='col-md-3 col-6 mt-3 text-center'>
-                        <h1 className='clr-white bold'>25<span style={{color:'#f5a005'}}>K</span></h1>
-                        <span className='clr-white bold'>Best Stories</span>
-                    </div>
-                    <div className='col-md-3 col-6 mt-3 text-center'>
-                        <h1 className='clr-white bold'>80<span style={{color:'#f5a005'}}>+</span></h1>
-                        <span className='clr-white bold'>Modern Tech</span>
-                    </div>
-                </div>
+        <div className='container-fluid strip-bg pt-5 pb-5'>
+            <h3 className='clr bold text-center'>OUR INDUSTRIES</h3>
+            <div className='container pt-4'>
+            <Swiper
+                breakpoints= {{
+                    480: {
+                      slidesPerView: 1,
+                      spaceBetween: 20
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20
+                      },
+                    1120: {
+                        slidesPerView: 3,
+                        spaceBetween: 20
+                      }
+                }}
+                modules={[Autoplay]}
+                loop={true}
+                autoplay={{delay: 4000,
+                disableOnInteraction: false}}
+                pagination={{ clickable: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)} className="banner-section">
+                {/* <SwiperSlide>
+                    <img src={SliderImg} width="100%"></img>
+                    <h1 style={{position:'relative', marginTop:'-100px', color:'#fff', textAlign:'center'}}>Be Connected</h1>
+                </SwiperSlide> */}
+                { industry_slider }
+            </Swiper>
             </div>
         </div>
         </>
